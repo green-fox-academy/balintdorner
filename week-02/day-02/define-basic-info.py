382cb73d4551bdb0516071f36928295bfd6e193a
Balint Dorner <dorner.balint@gmail.com>
2017-03-21 14:04:07 +0100

    reached exercise BMI

---
+++ week-02/day-02/define-basic-info.py
@@ -0,0 +1,6 @@
+myName = "Balint"
+myAge = 26
+myHeight = 1.75
+married = False
+
+print(myName, myAge, myHeight, married)
---
+++ week-02/day-02/favorite-number.py
@@ -0,0 +1,2 @@
+favorite_number = 8
+print("My favourite number is: " + str(favorite_number))
