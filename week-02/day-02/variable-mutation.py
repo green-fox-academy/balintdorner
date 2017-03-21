60afb8c1fc82d51980a97997c9ec867237082dda
Balint Dorner <dorner.balint@gmail.com>
2017-03-21 15:57:15 +0100

    number 13. a to k

---
+++ week-02/day-02/variable-mutation.py
@@ -0,0 +1,76 @@
+a = 3
+a += 10
+
+print(a)
+
+
+b = 100
+b -= 7
+
+print(b)
+
+
+c = 44
+c *= 2
+
+print(c)
+
+
+d = 125
+d //= 5
+
+print(d)
+
+
+e = 8
+e **= 4
+
+print(e)
+
+f1 = 123
+f2 = 345
+
+if f1 > f2:
+    print("True")
+else:
+    print("False")
+
+
+
+g1 = 350
+g2 = 200
+
+if g1 < g2*2:
+    print("True")
+else:
+    print("False")
+
+
+h = 1357988018575474
+
+if h%11 == 0:
+    print("True")
+else:
+    print("False")
+
+
+i1 = 10
+i2 = 3
+
+if i1 > i2**2 and i1 < i2**4:
+    print("True")
+else:
+    print("False")
+
+
+j = 1521
+
+if j%3 == 0 or j%5 == 0:
+    print("True")
+else:
+    print("False")
+
+
+k = "Apple"
+
+print(k*4)    
