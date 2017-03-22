b37acae0df50411c37d827b5c40e51c5f2c865df
Balint Dorner <dorner.balint@gmail.com>
2017-03-22 11:03:35 +0100

    first round is done. doubling with function, printing with list

--- week-02/day-02/draw-pyramid.py
+++ week-02/day-02/draw-pyramid.py
@@ -1,6 +1,10 @@
-print("Please enter a number")
-number = input()
-a = 1
-while a <= int(number):
-    print("*"*a)
-    a += 1
+# print("Please enter a number")
+#number = input()
+#a = 1
+#while a <= int(number):
+#    print("*"*a)
+#    a += 1
+
+
+for i in range(1,10):
+   print (" "*(10-i)+"*"*i+"*"*(i-1))
