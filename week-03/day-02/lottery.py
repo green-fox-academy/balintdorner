ba459ff724045cddbb81d40c9e1c0e5a60baee29
Balint Dorner <dorner.balint@gmail.com>
2017-03-28 17:18:20 +0200

    still trying figure out the most frquent lottery numbers

--- week-03/day-02/lottery.py
+++ week-03/day-02/lottery.py
@@ -1,8 +1,9 @@
 # Create a method that returns the five most frequent lottery number in a pretty table format
 myfile = open("otos.txt", "r")
 text = myfile.readlines()
-def five_most_frequent():
-    numbers = text[-1:5]
-    return numbers
-    pass
-print(five_most_frequent())
+
+for j in range(len(text)):
+    for i in range(4):
+        while text[j] == "t":
+            text[j] = ""
+print(text)
