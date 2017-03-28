03fca2bdc899aada87596f30cf63913fc975c336
Balint Dorner <dorner.balint@gmail.com>
2017-03-28 11:38:59 +0200

    .read moved into the function

--- week-03/day-02/crypto_1dup.py
+++ week-03/day-02/crypto_1dup.py
@@ -1,6 +1,5 @@
 # Create a method that decrypts the texts/duplicated_chars.txt
 myfile = open("duplicated_chars.txt", "r")
-text = myfile.read()
 
 def decrypt(file_name):
 
@@ -13,6 +12,7 @@ def decrypt(file_name):
 #        index += text[2*i]
 #    print(index)
 #    pass
+    text = myfile.read()
     printing = ""
     for char in range(len(text)):
         if char % 2 == 0:
