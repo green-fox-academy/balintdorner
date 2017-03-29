e5f4e3534219b2450a57f8ae677a73f2525c8366
Balint Dorner <dorner.balint@gmail.com>
2017-03-29 10:26:21 +0200

    line drawer function created

---
+++ week-03/day-03/go-to-center.py
@@ -0,0 +1,17 @@
+# create a line drawing function that takes 2 parameters:
+# the x and y coordinates of the line's starting point
+# and draws a line from that point to the center of the canvas.
+# draw 3 lines with that function.
+
+from tkinter import *
+
+root = Tk()
+
+canvas = Canvas(root, width='300', height='300')
+canvas.pack()
+
+def linedrawer(x_zero, y_zero):
+    canvas.create_line(x_zero, y_zero, 150, 150, fill ='green')
+    return
+linedrawer(0, 0)
+root.mainloop()
