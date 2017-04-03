acec6f8e47fe455b1fdcee474c1517f77965015c
Balint Dorner <dorner.balint@gmail.com>
2017-04-03 14:16:13 +0200

    animal eating, drinking and playing

---
+++ week-04/day-01/animal.py
@@ -0,0 +1,32 @@
+#Create Animal class
+#Every animal has a hunger value, which is a whole number
+#Every animal has a thirst value, which is a whole number
+#when creating a new animal object these values are created with the default 50 value
+#Every animal can eat() which decreases their hunger by one
+#Every animal can drink() which decreases their thirst by one
+#Every animal can play() which increases both by one
+
+class Animal(object):
+
+    def __init__(self, hunger=50 , thirst=50):
+        self.hunger = hunger
+        self.thirst = thirst
+
+    def eat(self):
+        self.hunger -= 1
+        return self.hunger
+
+    def drink(self):
+        self.thirst -= 1
+        return self.thirst
+
+    def play(self):
+        self.hunger += 1
+        self.thirst += 1
+        return self.hunger, self.thirst
+
+animal = Animal()
+
+print(animal.eat())
+print(animal.drink())
+print(animal.play())
