1a622642f016b687c5c7b4610713370b08983078
Balint Dorner <dorner.balint@gmail.com>
2017-04-03 17:56:11 +0200

    sharpieset, under construction

---
+++ week-04/day-01/sharpie-set.py
@@ -0,0 +1,43 @@
+#Reuse your Sharpie class
+#Create SharpieSet class
+#it contains a list of Sharpie
+#count_usable() -> sharpie is usable if it has ink in it
+#remove_trash() -> removes all unusable sharpies
+import random
+
+class Sharpie(object):
+
+    def __init__(self, color, width, ink_amount = 10):
+        self.color = color
+        self.width = float(width)
+        self.ink_amount = float(ink_amount)
+
+    def use(self):
+        random_usage = [5, 6, 7, 8, 9, 10, 11, 12, 13, 14, 15]
+        x = random.choice(random_usage)
+        self.ink_amount -= x
+
+        return self.ink_amount
+
+sharpie1 = Sharpie('red', 1)
+sharpie2 = Sharpie('blue', 2)
+sharpie3 = Sharpie('green', 3)
+sharpie4 = Sharpie('yellow', 4)
+sharpie5 = Sharpie('orange', 5)
+
+sharpie_list = [sharpie1.use(), sharpie2.use(), sharpie3.use(), sharpie4.use(), sharpie5.use()]
+print(sharpie_list)
+
+class Sharpieset(object):
+
+    def __init__(self):
+        x = Sharpie.sharpie_list
+
+    def counter(self):
+        y = 0
+        for i in range(len(x)):
+            if x[i]> 0:
+                y += 1
+
+sharpieset = SharpieSet()
+print(sharpieset.counter)
--- week-04/day-01/sharpie.py
+++ week-04/day-01/sharpie.py
@@ -4,18 +4,26 @@
 #Every sharpie is created with a default 100 as ink_amount
 #We can use() the sharpie objects
 #which decreases inkAmount
+import random
 
 class Sharpie(object):
 
-    def __init__(self, color, width, ink_amount = 100):
+    def __init__(self, color, width, ink_amount = 10):
         self.color = color
         self.width = float(width)
         self.ink_amount = float(ink_amount)
 
     def use(self):
-        self.ink_amount -= 1
+        random_usage = [5, 6, 7, 8, 9, 10, 11, 12, 13, 14, 15]
+        x = random.choice(random_usage)
+        self.ink_amount -= x
         return self.ink_amount
 
-sharpie = Sharpie('red', 200.5)
+sharpie1 = Sharpie('red', 200.5)
+sharpie2 = Sharpie('red', 200.5)
+sharpie3 = Sharpie('red', 200.5)
+sharpie4 = Sharpie('red', 200.5)
+sharpie5 = Sharpie('red', 200.5)
 
-print(sharpie.color, sharpie.width, sharpie.use())
+print(sharpie1.color, sharpie1.width, sharpie1.use())
+print(sharpie2.color, sharpie2.width, sharpie2.use())
