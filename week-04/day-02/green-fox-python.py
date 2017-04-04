d2a69004fe797d7e2353bff7eb50c8afe064b958
Balint Dorner <dorner.balint@gmail.com>
2017-04-04 11:11:20 +0200

    greenfox org chart, Person and Student done

---
+++ week-04/day-02/green-fox-python.py
@@ -0,0 +1,31 @@
+class Person(object):
+    def __init__(self, name = 'Jane Doe', age = 30, gender = 'female'):
+        self.name = name
+        self.age = age
+        self.gender = gender
+
+    def introduce(self):
+        print("Hi, I'm " + self.name + " , a " + str(self.age) + " year old " + self.gender + ".")
+
+    def get_goal(self):
+        print("My goal is: Live for the moment!")
+
+class Student(Person):
+    def __init__(self, name = 'Jane Doe', age = 30, gender = 'female', prev_org = 'The School of life', skipped_days = 0):
+        super().__init__(name, age, gender)
+        self.prev_org = prev_org
+        self.skipped_days = skipped_days
+
+    def get_goal(self):
+        print("Be a junior software developer.")
+
+    def introduce(self):
+        print("Hi, I'm " + self.name + " , a " + str(self.age) + " year old " + self.gender + " from " + self.prev_org + " who skipped " + str(self.skipped_days) + " days from the course already. ")
+
+
+person = Person()
+student = Student()
+person.introduce()
+person.get_goal()
+student.introduce()
+student.get_goal()
