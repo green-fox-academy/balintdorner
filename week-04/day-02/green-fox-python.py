d1af686f5d4c53d783b5adec85796c2db6cab866
Balint Dorner <dorner.balint@gmail.com>
2017-04-04 11:34:37 +0200

    greenfox org chart, Mentor is also included

--- week-04/day-02/green-fox-python.py
+++ week-04/day-02/green-fox-python.py
@@ -22,10 +22,34 @@ class Student(Person):
     def introduce(self):
         print("Hi, I'm " + self.name + " , a " + str(self.age) + " year old " + self.gender + " from " + self.prev_org + " who skipped " + str(self.skipped_days) + " days from the course already. ")
 
+    def skip_days(self, number_of_days):
+        self.number_of_days = number_of_days
+        self.skipped_days += self.number_of_days
+        return self.skipped_days
+
+class Mentor(Person):
+    def __init__(self, name = 'Jane Doe', age = 30, gender = 'female', level = 'Junior'):
+        super().__init__(name, age, gender)
+        self.level = level
+
+    def introduce(self):
+        print("Hi, I'm " + self.name + " , a " + str(self.age) + " year old " + self.gender + " " + str(self.level) + " mentor.")
+
+    def get_goal(self):
+        print("Educate brilliant junior software developers.")
+
+
+
+
 
 person = Person()
 student = Student()
+mentor = Mentor()
+#person.gender = 'who knows'
 person.introduce()
 person.get_goal()
 student.introduce()
 student.get_goal()
+student.skip_days(5)
+mentor.level = 'Intermediate'
+mentor.introduce()
