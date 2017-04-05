482755d0e55e25d76445e9a12e196fd4dfc293b0
Balint Dorner <dorner.balint@gmail.com>
2017-04-05 13:32:40 +0200

    unit testing: task sum all tests passed

--- week-04/day-03/balint_dorner_test.py
+++ week-04/day-03/balint_dorner_test.py
@@ -2,9 +2,30 @@ import unittest
 from balint_dorner_work import *
 
 class TestApple(unittest.TestCase):
-        def test_get_apple(self):
-            apple = Apple()
-            self.assertEqual(apple.get_apple(), 'apple')
+    def test_get_apple(self):
+        apple = Apple()
+        self.assertEqual(apple.get_apple(), 'apple')
+
+class TestSumList(unittest.TestCase):
+    def test_summa_empty_list(self, integer = []):
+        summa = SumList()
+        integer = []
+        self.assertEqual(summa.sum(integer), 0)
+
+    def test_summa_one_element(self, integer = []):
+        summa = SumList()
+        integer = [5]
+        self.assertEqual(summa.sum(integer), 5)
+
+    def test_summa_multiple_element(self, integer = []):
+        summa = SumList()
+        integer = [5, 6, 7]
+        self.assertEqual(summa.sum(integer), 18)
+
+    def test_summa_null(self, integer = []):
+        summa = SumList()
+        integer = [0]
+        self.assertEqual(summa.sum(integer), 0)
 
 
 if __name__ == '__main__':
--- week-04/day-03/balint_dorner_work.py
+++ week-04/day-03/balint_dorner_work.py
@@ -1,3 +1,8 @@
 class Apple():
     def get_apple(self):
         return 'apple'
+
+class SumList():
+    def sum(self, integer = []):
+        self.integer = integer
+        return sum(self.integer)
