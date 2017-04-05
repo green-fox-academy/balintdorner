11d175cda0767f2bbed5d78ad86a4be632117b2f
Balint Dorner <dorner.balint@gmail.com>
2017-04-05 13:09:36 +0200

    unit testing: task apples tested

---
+++ week-04/day-03/balint_dorner_test.py
@@ -0,0 +1,11 @@
+import unittest
+from balint_dorner_work import *
+
+class TestApple(unittest.TestCase):
+        def test_get_apple(self):
+            apple = Apple()
+            self.assertEqual(apple.get_apple(), 'apple')
+
+
+if __name__ == '__main__':
+    unittest.main()
