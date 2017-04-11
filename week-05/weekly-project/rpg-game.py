47884ef8cea76744ada6e0a89dbedf3e88fa3dd3
Balint Dorner <dorner.balint@gmail.com>
2017-04-11 17:01:50 +0200

    background is finalized

--- week-05/weekly-project/rpg-game.py
+++ week-05/weekly-project/rpg-game.py
@@ -2,7 +2,7 @@ from tkinter import *
 
 class Box(object):
     def __init__(self):
-        self.testBoxX = 65
+        self.testBoxX = 72
         self.testBoxY = 0
         self.background = [
         [1, 1, 1, 0, 1, 0, 1, 1, 1, 1],
@@ -11,11 +11,13 @@ class Box(object):
         [1, 1, 1, 1, 1, 0, 1, 1, 1, 1],
         [0, 0, 0, 0, 1, 0, 0, 0, 0, 1],
         [1, 0, 1, 0, 1, 1, 1, 1, 0, 1],
+        [1, 0, 1, 0, 1, 0, 0, 1, 0, 1],
         [1, 1, 1, 1, 1, 1, 0, 0, 0, 1],
         [1, 0, 0, 0, 1, 1, 1, 1, 0, 1],
         [1, 1, 1, 0, 1, 0, 0, 1, 0, 1],
         [1, 0, 1, 0, 1, 0, 1, 1, 1, 1],
         ]
+        self.map()
 
     def draw(self, canvas):
         self.main_character = PhotoImage(file = 'hero-down.png')
@@ -24,18 +26,17 @@ class Box(object):
     def map(self):
         self.wall = PhotoImage(file = 'wall.png')
         self.floor = PhotoImage(file = 'floor.png')
-        for line in self.background:
-            for j in range(10):
-                if self.background[line][j] == 1:
-                    canvas.create_image(line*75, j*75, image = self.floor)
+        for line in range(len(self.background)):
+            for element in range(len(self.background[line])):
+                if self.background[line][element] == 1:
+                    canvas.create_image(element*72, line*72, anchor = NW, image = self.floor)
                 else:
-                    canvas.create_image(line*75, j*75, image = 'wall.png')
-        root.mainloop()
+                    canvas.create_image(element*72, line*72, anchor = NW, image = self.wall)
 
 
 # Create the tk environment as usual
 root = Tk()
-canvas = Canvas(root, width=750, height=825)
+canvas = Canvas(root, width=720, height=792)
 
 # Creating a box that can draw itself in a certain position
 box = Box()
@@ -46,21 +47,20 @@ class MainLoop():
         # When the keycode is 111 (up arrow) we move the position of our box higher
         if e.keycode == 38:
             if box.testBoxY > 0:
-                box.testBoxY = box.testBoxY - 75
+                box.testBoxY = box.testBoxY - 72
         elif e.keycode == 40:
-            if box.testBoxY < 750:
-                box.testBoxY = box.testBoxY + 75
+            if box.testBoxY < 720:
+                box.testBoxY = box.testBoxY + 72
         elif e.keycode == 39:
-            if box.testBoxX < 675:
-                box.testBoxX = box.testBoxX + 75
+            if box.testBoxX <= 648:
+                box.testBoxX = box.testBoxX + 72
         elif e.keycode == 37:
-            if box.testBoxX > 75:
-                box.testBoxX = box.testBoxX - 75
+            if box.testBoxX > 72:
+                box.testBoxX = box.testBoxX - 72
 
         # and lower if the key that was pressed the down arrow
         # draw the box again in the new position
         box.draw(canvas)
-        box.map()
 mainloop = MainLoop()
 # Tell the canvas that we prepared a function that can deal with the key press events
 canvas.bind("<KeyPress>", mainloop.on_key_press)
@@ -71,5 +71,4 @@ canvas.focus_set()
 
 # Draw the box in the initial position
 box.draw(canvas)
-box.map()
 root.mainloop()
