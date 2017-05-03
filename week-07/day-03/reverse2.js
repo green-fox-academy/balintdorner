95ed2a34f98c803dce3b680e04c9e83d990f7d83
Balint Dorner <dorner.balint@gmail.com>
2017-05-03 16:23:35 +0200

    strings exercises are done

---
+++ week-07/day-03/reverse2.js
@@ -0,0 +1,13 @@
+'use strict';
+// Reverse the string!
+
+var reversed = ".eslaf eb t'ndluow ecnetnes siht ,dehctiws erew eslaf dna eurt fo sgninaem eht fI";
+var text = '';
+
+for (var i = reversed.length-1 ;i >= 0 ; i--) {
+    text += reversed[i]
+}
+console.log(text)
+
+
+// str.split("").reverse().join("");
