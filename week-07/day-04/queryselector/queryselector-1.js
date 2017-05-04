aa4e21e0558704423360b93e502485d6abf66f55
Balint Dorner <dorner.balint@gmail.com>
2017-05-04 11:54:22 +0200

    exercise: queryselector-1 is done

---
+++ week-07/day-04/queryselector/queryselector-1.js
@@ -0,0 +1,21 @@
+var king = document.getElementById('b325');
+console.log(king);
+
+var conceited = document.querySelector('.b326');
+alert(conceited);
+
+var businesslamp = document.querySelectorAll('.big')
+console.log(businesslamp[0])
+console.log(businesslamp[1])
+
+var conceitedKing = document.querySelectorAll('section div');
+alert(conceitedKing[0]);
+alert(conceitedKing[1]);
+
+var noBusiness = document.getElementsByTagName('div');
+for (var i = 0;i < noBusiness.length ; i++) {
+    console.log(noBusiness[i]);
+}
+
+var allBizniss = document.getElementsByTagName('p');
+console.log(allBizniss[0]);
