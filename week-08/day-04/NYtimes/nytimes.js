f7686f7e881f29df0206bc5130fa34785c3b41b9
Balint Dorner <dorner.balint@gmail.com>
2017-05-11 14:03:36 +0200

    nytimes h1 and p and p is done

--- week-08/day-04/NYtimes/nytimes.js
+++ week-08/day-04/NYtimes/nytimes.js
@@ -8,7 +8,6 @@ let list;
 xhr.onreadystatechange = function() {
     if (xhr.readyState === XMLHttpRequest.DONE) {
         list = JSON.parse(xhr.response)
-        console.log(list)
     writeText()
     }
 }
@@ -17,21 +16,29 @@ var getDiv = document.querySelector('div');
 var paragraph;
 var pubDate;
 var headline;
+var permaLink;
 
 function writeText() {
     for (let i = 0; i < 10; i++) {
         headline = document.createElement('h1');
-         paragraph = document.createElement('p');
-         pubDate = document.createElement('p');
+        paragraph = document.createElement('p');
+        pubDate = document.createElement('p');
+        permaLink = document.createElement('a')
         var headlineText = list.response.docs[i].headline.main;
         var paragraphText = list.response.docs[i].snippet;
         var pubDateText = list.response.docs[i].pub_date;
+        var permaLinkSource = list.response.docs[i].web_url;
+        var permaLinkText = list.response.docs[i].source;
         headline.innerText = headlineText;
         paragraph.innerText = paragraphText;
         pubDate.innerText = pubDateText;
+        permaLink.setAttribute('href', permaLinkSource)
+        permaLink.innerText = permaLinkText
         getDiv.appendChild(headline);
         getDiv.appendChild(paragraph);
         getDiv.appendChild(pubDate);
+        getDiv.appendChild(permaLink);
+        console.log(permaLink);
     }
 
 }
