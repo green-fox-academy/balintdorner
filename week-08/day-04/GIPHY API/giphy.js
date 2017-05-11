c90531c52c568186e240425ca7987b8f15db320f
Balint Dorner <dorner.balint@gmail.com>
2017-05-11 13:14:22 +0200

    giphy exercise is done-updated

--- week-08/day-04/GIPHY API/giphy.js
+++ week-08/day-04/GIPHY API/giphy.js
@@ -1,24 +1,31 @@
-var xhr = new XMLHttpRequest();
+let xhr = new XMLHttpRequest();
 
 xhr.open('GET', 'http://api.giphy.com/v1/stickers/search?q=city&api_key=dc6zaTOxFJmzC', true);
 
 xhr.send();
-var list;
+let list;
 
 xhr.onreadystatechange = function() {
     if (xhr.readyState === XMLHttpRequest.DONE) {
         list = JSON.parse(xhr.response)
+        console.log(list)
         creatGif()
     }
 }
 
-var getDiv = document.querySelector('div');
+let getDiv = document.querySelector('div');
+let newImage;
 
 function creatGif() {
-        for (var i = 0; i < 16; i++) {
-        var newImage = document.createElement('img');
-        var getGifUrl = list.data[i].images.original.url;
-        newImage.setAttribute('src', getGifUrl)
+        for (let i = 0; i < 16; i++) {
+        let newImage = document.createElement('img');
+        let getImgUrl = list.data[i].images.original_still.url;
+        newImage.setAttribute('src', getImgUrl)
+        newImage.addEventListener('click', function() {
+            let getGifURL = list.data[i].images.original.url;
+            newImage.setAttribute('src', getGifURL)
+            console.log('alma')
+        });
         getDiv.appendChild(newImage)
     }
 }
