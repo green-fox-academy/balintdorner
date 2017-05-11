163a650a31d80af81f5bc6384a9fe8503e78c113
Balint Dorner <dorner.balint@gmail.com>
2017-05-11 16:58:12 +0200

    get position is done+map is also done

--- week-08/day-04/GetPosition/getPosition.js
+++ week-08/day-04/GetPosition/getPosition.js
@@ -4,26 +4,35 @@ let xhr = new XMLHttpRequest();
 let input = document.querySelector('input')
 let search = document.querySelector('button')
 let frame = document.querySelector('iframe')
+let getDiv = document.querySelector('section');
+let list;
 let city = 'New+York'
 
-function searching() {
+search.addEventListener('click', function() {
+
+    xhr.open('GET', 'https://devru-latitude-longitude-find-v1.p.mashape.com/latlon.php?location=' + city, true);
+
+    xhr.setRequestHeader("X-Mashape-Key", "wUWumAJ8mhmsh0bM1lGXzWyf0sF8p1wEUoejsntiavnGjjTZVs")
+    xhr.setRequestHeader("Accept", "application/json")
+
+    xhr.send();
+
+    xhr.onreadystatechange = function() {
+        if (xhr.readyState === XMLHttpRequest.DONE) {
+            list = JSON.parse(xhr.response)
+            console.log(list)
+            var latitude = document.createElement('p');
+            var longitude = document.createElement('p');
+            var latitudeText = list.Results[0].lat;
+            var longitudeText = list.Results[0].lon;
+            latitude.innerText = latitudeText;
+            longitude.innerText = longitudeText;
+            getDiv.appendChild(latitude);
+            getDiv.appendChild(longitude);
+        }
+    }
+
     console.log(input.value)
     city = (input.value)
-    frame.src = 'https://www.google.com/maps/embed/v1/place?key=AIzaSyB_3Rf-W8VMLQQcFm7mrT2ZOjldoEvfLec&q='+ city
-}
-
-search.addEventListener('click', searching)
-
-xhr.open('GET', 'https://devru-latitude-longitude-find-v1.p.mashape.com/latlon.php?location=' + city, true);
-
-xhr.setRequestHeader("X-Mashape-Key", "wUWumAJ8mhmsh0bM1lGXzWyf0sF8p1wEUoejsntiavnGjjTZVs")
-xhr.setRequestHeader("Accept", "application/json")
-
-xhr.send();
-
-xhr.onreadystatechange = function() {
-    if (xhr.readyState === XMLHttpRequest.DONE) {
-        list = JSON.parse(xhr.response)
-        console.log(list)
-    }
-}
+    frame.src = 'https://www.google.com/maps/embed/v1/place?key=AIzaSyB_3Rf-W8VMLQQcFm7mrT2ZOjldoEvfLec&q='+ city;
+})
