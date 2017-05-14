61463a074cdfaaa6cd01cc5d55bfb247f978ba2e
Balint Dorner <dorner.balint@gmail.com>
2017-05-14 18:16:48 +0200

    buttons are added, working on functions

--- Personal/Basketball/bb.js
+++ Personal/Basketball/bb.js
@@ -1,7 +1,8 @@
 var whichPlayer = 1;
-var addPlayer = document.querySelector('.addplayer')
+var addPlayer; document.querySelector('.addplayer')
 
-var TwopointButton = document.querySelector('.twopoint')
+var closeShotMadeButton = document.querySelector('.closeshotmade')
+var closeShotMadeButton = document.querySelector('.closeshotmade')
 
 function playerAdder() {
     var input = document.querySelector('input')
@@ -14,56 +15,187 @@ function playerAdder() {
 
     var nameBox = document.createElement('div');
     var numberBox = document.createElement('div');
-    var shotBox = document.createElement('div');
-    var pointBox = document.createElement('div');
-    var assistBox = document.createElement('div');
-    var reboundBox = document.createElement('div');
-    var stealBox = document.createElement('div');
-    var blockBox = document.createElement('div');
+    var closeShotMadeBox = document.querySelector('div');
+    var closeShotMissedBox = document.querySelector('div');
+    var middleRangeShotMadeBox = document.querySelector('div');
+    var middleRangeShotMissedBox = document.querySelector('div');
+    var threepointShotMadeBox = document.querySelector('div');
+    var threepointShotMissedBox = document.querySelector('div');
+    var assistBox = document.querySelector('div');
+    var reboundBox = document.querySelector('div');
+    var stealBox = document.querySelector('div');
+    var blockBox = document.querySelector('div');
+    var turnoverBox = document.querySelector('div');
+
+    var closeShotMade = 0;
+    var closeShotMissed = 0;
+    var middleRangeShotMade = 0;
+    var middleRangeShotMissed = 0;
+    var threepointShotMade = 0;
+    var threepointShotMissed = 0;
+    var assist = 0;
+    var rebound = 0;
+    var steal = 0;
+    var block = 0;
+    var turnover = 0;
 
     nameBox.innerText = input.value;
     nameBox.className = 'player';
     nameBox.id = input.value;
-    var scoredPoint = 0;
-    shotBox.innerText = scoredPoint;
+    closeShotMadeBox.innerText = closeShotMade;
+    closeShotMissedBox.innerText = closeShotMissed;
+    middleRangeShotMadeBox.innerText = middleRangeShotMade;
+    middleRangeShotMissedBox.innerText = middleRangeShotMissed;
+    threepointShotMadeBox.innerText = threepointShotMade;
+    threepointShotMissedBox.innerText = threepointShotMissed;
+    assistBox.innerText = assist;
+    reboundBox.innerText = rebound;
+    stealBox.innerText = steal;
+    blockBox.innerText = block;
+    turnoverBox.innerText = turnover;
 
     player.appendChild(nameBox);
     player.appendChild(numberBox);
-    player.appendChild(shotBox);
-    player.appendChild(pointBox);
+    player.appendChild(closeShotMadeBox);
+    player.appendChild(closeShotMissedBox);
+    player.appendChild(middleRangeShotMadeBox);
+    player.appendChild(middleRangeShotMissedBox);
+    player.appendChild(threepointShotMadeBox);
+    player.appendChild(threepointShotMissedBox);
     player.appendChild(assistBox);
     player.appendChild(reboundBox);
     player.appendChild(stealBox);
     player.appendChild(blockBox);
 
-    function twoPointAdder() {
+    function CloseShotMader() {
         document.onclick = function(e) {
             if (e.target.className === 'player') {
                 playerId = e.target.id
             }
         }
         if (playerId === nameBox.id) {
-            scoredPoint +=2
-            shotBox.innerText = scoredPoint;
+            closeShotMade++
+            closeShotMadeBox.innerText = closeShotMade;
         }
     }
 
-    TwopointButton.addEventListener('click', twoPointAdder)
+    function CloseShotMisser() {
+        document.onclick = function(e) {
+            if (e.target.className === 'player') {
+                playerId = e.target.id
+            }
+        }
+        if (playerId === nameBox.id) {
+            closeShotMissed++
+            closeShotMissedBox.innerText = closeShotMissed;
+        }
+    }
+
+    function MiddleRangeShotMader() {
+        document.onclick = function(e) {
+            if (e.target.className === 'player') {
+                playerId = e.target.id
+            }
+        }
+        if (playerId === nameBox.id) {
+            middleRangeShotMade++
+            middleRangeShotMadeBox.innerText = middleRangeShotMade;
+        }
+    }
+
+    function MiddleRangeShotMisser() {
+        document.onclick = function(e) {
+            if (e.target.className === 'player') {
+                playerId = e.target.id
+            }
+        }
+        if (playerId === nameBox.id) {
+            middleRangeShotMissed++
+            middleRangeShotMissed.innerText = middleRangeShotMissed;
+        }
+    }
+
+    function ThreePointShotMader() {
+        document.onclick = function(e) {
+            if (e.target.className === 'player') {
+                playerId = e.target.id
+            }
+        }
+        if (playerId === nameBox.id) {
+            threepointShotMade++
+            threepointShotMadeBox.innerText = threepointShotMade;
+        }
+    }
+
+    function ThreePointShotMisser() {
+        document.onclick = function(e) {
+            if (e.target.className === 'player') {
+                playerId = e.target.id
+            }
+        }
+        if (playerId === nameBox.id) {
+            threepointShotMissed++
+            threepointShotMissedBox.innerText = threepointShotMissed;
+        }
+    }
+
+    function Assister() {
+        document.onclick = function(e) {
+            if (e.target.className === 'player') {
+                playerId = e.target.id
+            }
+        }
+        if (playerId === nameBox.id) {
+            assist++
+            assistBox.innerText = assist;
+        }
+    }
+
+    function Blocker() {
+        document.onclick = function(e) {
+            if (e.target.className === 'player') {
+                playerId = e.target.id
+            }
+        }
+        if (playerId === nameBox.id) {
+            block++
+            blockBox.innerText = block;
+        }
+    }
+
+    function Stealer() {
+        document.onclick = function(e) {
+            if (e.target.className === 'player') {
+                playerId = e.target.id
+            }
+        }
+        if (playerId === nameBox.id) {
+            steal++
+            stealBox.innerText = steal;
+        }
+    }
+
+    function Turnoverer() {
+        document.onclick = function(e) {
+            if (e.target.className === 'player') {
+                playerId = e.target.id
+            }
+        }
+        if (playerId === nameBox.id) {
+            turnover++
+            turnoverBox.innerText = turnover;
+        }
+    }
+
+    closeShotMadeButton.addEventListener('click', CloseShotMader)
+    closeShotMissedButton.addEventListener('click', CloseShotMisser)
+    middleRangeShotMadeButton.addEventListener('click', MiddlerRangeShotMader)
+    middleRangeShotMissedButton.addEventListener('click', MiddlerRangeShotMisser)
+    threepointShotMadeButton.addEventListener('click', ThreePointShotMader)
+    threepointShotMissedButton.addEventListener('click', ThreePointShotMisser)
+    assistButton.addEventListener('click', ThreePointShotMisser)
 }
 
 var playerId;
-
-/*function playerSelector() {
-    document.onclick = function(e) {
-        if (e.target.className === 'player') {
-            playerId = e.target.id
-        }
-    }
-}
-
-playerSelector()
-*/
-
-
 
 addPlayer.addEventListener('click', playerAdder)
