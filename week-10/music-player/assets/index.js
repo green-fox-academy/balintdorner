3e4c00ff2810649f321269db3afbbae44790c753
Balint Dorner <dorner.balint@gmail.com>
2017-05-24 16:12:08 +0200

    playlistRender is working based on the mock server

---
+++ week-10/music-player/assets/index.js
@@ -0,0 +1,39 @@
+'use strict';
+
+let xhr = new XMLHttpRequest();
+let url = 'http://localhost:3000/playlists';
+
+var getIndex = function() {
+    xhr.open('GET', url, true);
+    xhr.setRequestHeader("Accept", "application/json");
+    xhr.send();
+
+    xhr.onreadystatechange = function() {
+        let list;
+        if (xhr.readyState === XMLHttpRequest.DONE) {
+            list = JSON.parse(xhr.response)
+            console.log(list)
+            for (let k = 0; k <list.length; k++) {
+                playlistRender(list[k].title)
+            }
+
+        }
+    }
+}
+
+getIndex();
+
+function playlistRender(title) {
+
+    var playlistsContainer = document.querySelector('.playlist');
+    var playlistElement = document.createElement('div');
+    var playlistName = document.createElement('span');
+    var playlistDeleter = document.createElement('button');
+
+    playlistsContainer.appendChild(playlistElement);
+    playlistElement.appendChild(playlistName);
+    playlistElement.appendChild(playlistDeleter);
+
+    playlistName.innerText = title;
+    playlistDeleter.innerText = 'X';
+}
