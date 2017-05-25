218d4900583bc418f0643e82da0c162ce1401d74
Balint Dorner <dorner.balint@gmail.com>
2017-05-25 17:45:34 +0200

    tracklist selector is working on backend

--- week-10/music-player/assets/index.js
+++ week-10/music-player/assets/index.js
@@ -6,7 +6,25 @@ class Ajax {
         this.url = 'http://localhost:3000/';
     };
 
-    request(method, endpoint, callback) {
+    requestPlaylist(method, endpoint, callback) {
+        this.method = method;
+        this.endpoint = endpoint;
+        this.xhr.open(this.method, this.url + this.endpoint, true);
+        this.xhr.setRequestHeader("Accept", "application/json");
+        this.xhr.send();
+
+        this.xhr.onreadystatechange = function() {
+            this.response;
+            if (this.xhr.readyState === XMLHttpRequest.DONE) {
+                if (this.xhr.status === 200) {
+                    this.response = JSON.parse(this.xhr.response)
+                    callback(this.response.length, this.response);
+                };
+            };
+        }.bind(this);
+    };
+
+    requestTracklist(method, endpoint, callback) {
         this.method = method;
         this.endpoint = endpoint;
         this.xhr.open(this.method, this.url + this.endpoint, true);
@@ -28,10 +46,10 @@ class Ajax {
 class Render {
     playlistRender(length, title) {
         for (var i = 0; i < length; i++) {
-            var playlistsContainer = document.querySelector('.playlist');
-            var playlistElement = document.createElement('div');
-            var playlistName = document.createElement('span');
-            var playlistDeleter = document.createElement('button');
+            const playlistsContainer = document.querySelector('.playlist');
+            const playlistElement = document.createElement('div');
+            const playlistName = document.createElement('span');
+            const playlistDeleter = document.createElement('button');
 
             playlistsContainer.appendChild(playlistElement);
             playlistElement.appendChild(playlistName);
@@ -41,18 +59,55 @@ class Render {
             playlistDeleter.innerText = 'X';
         }
     };
+
+    tracklistRender(length, data) {
+        for (var i = 0; i < length; i++) {
+            const tracklistContainer =  document.querySelector('.tracklist');
+            const tracklistElement = document.createElement('div');
+            const trackID = document.createElement('span');
+            const trackTitle = document.createElement('span');
+            const trackDuration = document.createElement('span');
+
+            tracklistContainer.appendChild(tracklistElement);
+            tracklistElement.appendChild(trackID);
+            tracklistElement.appendChild(trackTitle);
+            tracklistElement.appendChild(trackDuration);
+
+            tracklistElement.className = 'track';
+            trackID.innerText = i+1;
+            trackTitle.innerText = data[i].title + ' ' + data[i].artist;
+            trackDuration.innerText = function() {
+                let minutes = parseInt(data[i].duration/60);
+                let seconds = parseInt(data[i].duration%60);
+                if (seconds < 10) {
+                    seconds = '0' + seconds;
+                };
+                if (minutes < 10) {
+                    minutes = '0' + minutes;
+                };
+                let timeMinSec = minutes + ':' + seconds;
+                return timeMinSec
+            }();
+        };
+    };
 };
 
 class Controller {
     constructor() {
-        this.ajax = new Ajax();
+        this.playlistAjax = new Ajax();
+        this.tracklistAjax = new Ajax();
         this.render = new Render();
     };
 
-    drawing() {
-        this.ajax.request('GET', 'playlists', this.render.playlistRender);
+    drawingPlaylist() {
+        this.playlistAjax.requestPlaylist('GET', 'playlists', this.render.playlistRender);
+    };
+
+    drawingTracklist() {
+        this.tracklistAjax.requestTracklist('GET', 'playlist-tracks', this.render.tracklistRender);
     };
 };
 
 var controller = new Controller();
-controller.drawing();
+controller.drawingPlaylist();
+controller.drawingTracklist();
--- week-10/music-player/server.js
+++ week-10/music-player/server.js
@@ -3,6 +3,7 @@
 const express = require('express');
 const cors = require('cors');
 const bodyParser = require('body-parser');
+const mysql = require('mysql');
 
 const playlist = [
     { "id": 1, "title": "Favorites", "system": 1},
@@ -16,6 +17,13 @@ const tracklist = [
 	{ "id": 412, "title": "No sleep till Brooklyn", "artist": "Beastie Boys", "duration": 312.12, "path": "c:/music/beastie boys/No sleep till Brooklyn.mp3"}
 ];
 
+const conn = mysql.createConnection({
+  host: "localhost",
+  user: "root",
+  password: "root",
+  database: "disco"
+});
+
 var app = express();
 app.use(bodyParser.json());
 app.use(cors());
@@ -26,11 +34,34 @@ app.get('/', function(req, res) {
 });
 
 app.get('/playlists', function(req, res) {
-    res.send(playlist);
+    conn.query("SELECT * FROM playlists",function(err,rows){
+        if(err) {
+            console.log("PARA", err.message);
+        }
+        res.send(rows);
+    });
 });
 
 app.get('/playlist-tracks', function(req, res) {
-    res.send(tracklist);
+    var id = req.query.id;
+    var item;
+    conn.query("SELECT * FROM tracklist",function(err,rows){
+        if(err) {
+            console.log("PARA", err.message);
+        } else {
+            if (id === undefined) {
+                res.send(rows);
+            } else {
+                rows.forEach(row => {
+                    if( id == row.ID) {
+                        item = row;
+
+                    }
+                });
+            } res.send(item);
+        }
+
+    });
 });
 
 app.listen(3000, function() {
