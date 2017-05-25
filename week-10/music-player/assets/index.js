79847e27146ac2c33b3162b54c8b0a50f8f00288
Balint Dorner <dorner.balint@gmail.com>
2017-05-25 14:40:51 +0200

    object oriented solution is working

--- week-10/music-player/assets/index.js
+++ week-10/music-player/assets/index.js
@@ -1,44 +1,48 @@
 'use strict';
-console.log('cica')
+
 class Ajax {
     constructor() {
         this.xhr = new XMLHttpRequest();
         this.url = 'http://localhost:3000/';
-        console.log('alma');
     };
 
     request(method, endpoint, callback) {
         this.method = method;
         this.endpoint = endpoint;
-        this.xhr.open(this.method, this.url+this.endpoint, true);
+        this.xhr.open(this.method, this.url + this.endpoint, true);
         this.xhr.setRequestHeader("Accept", "application/json");
         this.xhr.send();
 
         this.xhr.onreadystatechange = function() {
             this.response;
             if (this.xhr.readyState === XMLHttpRequest.DONE) {
-                this.response = JSON.parse(this.xhr.response)
-                callback(this.response);
+                if (this.xhr.status === 200) {
+                    this.response = JSON.parse(this.xhr.response)
+                    callback(this.response.length, this.response);
+                };
+            };
+        }.bind(this);
     };
 };
 
 class Render {
-    playlistRender(title) {
+    playlistRender(length, title) {
+        for (var i = 0; i < length; i++) {
+            var playlistsContainer = document.querySelector('.playlist');
+            var playlistElement = document.createElement('div');
+            var playlistName = document.createElement('span');
+            var playlistDeleter = document.createElement('button');
 
-        var playlistsContainer = document.querySelector('.playlist');
-        var playlistElement = document.createElement('div');
-        var playlistName = document.createElement('span');
-        var playlistDeleter = document.createElement('button');
+            playlistsContainer.appendChild(playlistElement);
+            playlistElement.appendChild(playlistName);
+            playlistElement.appendChild(playlistDeleter);
 
-        playlistsContainer.appendChild(playlistElement);
-        playlistElement.appendChild(playlistName);
-        playlistElement.appendChild(playlistDeleter);
-
-        playlistName.innerText = title;
-        playlistDeleter.innerText = 'X';
+            playlistName.innerText = title[i].title;
+            playlistDeleter.innerText = 'X';
+        }
     };
 };
-
+
 class Controller {
     constructor() {
         this.ajax = new Ajax();
