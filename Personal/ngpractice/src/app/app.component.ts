682ee19291af788362744516e53b79bd769f9c04
Balint Dorner <dorner.balint@gmail.com>
2017-06-11 00:28:23 +0200

    feat(userapp): one, two, twoway binding included

--- Personal/ngpractice/src/app/app.component.ts
+++ Personal/ngpractice/src/app/app.component.ts
@@ -1,43 +1,23 @@
 import { Component } from '@angular/core';
+import { User } from './shared/models/users';
 
 @Component({
   selector: 'my-app',
-  template: `
-  <header>
-    <nav class="navbar navbar-inverse">
-      <div class="navbar-header">
-        <a href="/" class="navbar-brand">My Angular 2</a>     
-      </div>
-    </nav>  
-  </header>
-
-  <main>
-    <div class="jumbotron">
-        <h1>Welcome to our App!</h1>
-        <p>{{ message }}</p>
-    </div>
-    
-    <div *ngIf="users">
-      <div *ngFor="let user of users">
-        {{ user.name }} ({{ user.username }})
-      </div>
-    </div>
-
-  </main>
-
-  <footer class="text-center">
-    Copyright &copy; 2017
-  </footer>`,
-  styles: [`
-    .jumbotron { 
-      box-shadow: 0 2px 0 rgba(0, 0, 0, 0.2); }
-  `]
+  templateUrl: './app.component.html',
+  styleUrls: ['./app.component.css']
 })
+
 export class AppComponent {
-  message = 'Hello';
-  users = [
+  message: string = 'Hello';
+  users: User[] = [
     { id: 26, name: 'Babafej', username: 'Szerelem'},
     { id: 27, name: 'Babifej2', username: 'Cukibaba'},
     { id: 28, name: 'Babafej3', username: 'Cicamica'}
-    ]
+  ];
+  activeUser: User;
+
+  selectUser(user) {
+    this.activeUser = user;
+    
+  }
 }
