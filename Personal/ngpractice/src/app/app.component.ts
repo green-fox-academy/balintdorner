170741ebc4646960d7537f1702849b1c0c5c9463
Balint Dorner <dorner.balint@gmail.com>
2017-06-10 22:28:12 +0200

    feat(users): user list created, and added to the site

--- Personal/ngpractice/src/app/app.component.ts
+++ Personal/ngpractice/src/app/app.component.ts
@@ -2,11 +2,42 @@ import { Component } from '@angular/core';
 
 @Component({
   selector: 'my-app',
-  templateUrl: `./app.component.html`,
+  template: `
+  <header>
+    <nav class="navbar navbar-inverse">
+      <div class="navbar-header">
+        <a href="/" class="navbar-brand">My Angular 2</a>     
+      </div>
+    </nav>  
+  </header>
+
+  <main>
+    <div class="jumbotron">
+        <h1>Welcome to our App!</h1>
+        <p>{{ message }}</p>
+    </div>
+    
+    <div *ngIf="users">
+      <div *ngFor="let user of users">
+        {{ user.name }} ({{ user.username }})
+      </div>
+    </div>
+
+  </main>
+
+  <footer class="text-center">
+    Copyright &copy; 2017
+  </footer>`,
   styles: [`
     .jumbotron { 
-      background-color: green;
       box-shadow: 0 2px 0 rgba(0, 0, 0, 0.2); }
   `]
 })
-export class AppComponent {}
+export class AppComponent {
+  message = 'Hello';
+  users = [
+    { id: 26, name: 'Babafej', username: 'Szerelem'},
+    { id: 27, name: 'Babifej2', username: 'Cukibaba'},
+    { id: 28, name: 'Babafej3', username: 'Cicamica'}
+    ]
+}
