83c7b9fd75ddecb3c177357cdafe8052371b10a4
Balint Dorner <dorner.balint@gmail.com>
2017-06-12 15:51:27 +0200

    refactor(poker.js): functions splitted into different functions

--- week-12/day-01/poker.js
+++ week-12/day-01/poker.js
@@ -1,10 +1,9 @@
 'use strict';
 
-const handChecker = function(hand1) {
+const slicer = function(hand1) {
     let number = [];
     let type = [];
-    let isStraightFlush = true;
-    hand1.forEach(function(e) {
+     hand1.forEach(function(e) {
         number.push(e.slice(0, 1));
         type.push(e.slice(1));    
     })
@@ -13,20 +12,29 @@ const handChecker = function(hand1) {
     number.forEach(function(e, i) {
         number[i] = parseInt(e)
     })
+    return number, type
+}
+
+const straightChecker = function(number) {
+    let isStraight = true;
 
     for(let i = 0; i < number.length-1; i++) {
         if (number[i+1] !== number[i]+1) {  
-            isStraightFlush = false;
+            isStraight = false;
         }
     }
+    return isStraight
+}
 
+const flushChecker = function(type) {     
+    let isFlush = true;
+   
     for(let i = 0; i < type.length-1; i++) {
         if (type[i+1] !== type[i]) {  
-            isStraightFlush = false;
+            isFlush = false;
         }
     }
-
-    return isStraightFlush
+    return isFlush
 }
 
 handChecker(["5H", "2H", "4H", "2H", "6H"])
