35c6536266e20e6063d3c34dfdbe182d7e9f7935
Balint Dorner <dorner.balint@gmail.com>
2017-06-12 15:33:09 +0200

    feat(poker.js): straight checker works

--- week-12/day-01/poker.js
+++ week-12/day-01/poker.js
@@ -1,9 +1,35 @@
 'use strict';
 
 const handChecker = function(hand1) {
-    
+    let number = [];
+    let intNumber = []
+    let type = [];
+    let isStraight = true;
+    hand1.forEach(function(e) {
+        number.push(e.slice(0, 1));
+        type.push(e.slice(1));    
+    })
+
+    number = number.sort();
+    number.forEach(function(e, i) {
+        number[i] = parseInt(e)
+    })
+
+    for(let i = 0; i < number.length-1; i++) {
+        console.log(number[i]+1)
+        console.log(number[i+1])
+        if (number[i+1] !== number[i]+1) {  
+            isStraight = false;
+        }
+    }
+
+    console.log(number);
+    console.log(type);
+    console.log(isStraight);
+    return isStraight
 }
 
+handChecker(["5H", "2H", "4H", "2H", "6H"])
 
 /*  Magas lap
     Par
