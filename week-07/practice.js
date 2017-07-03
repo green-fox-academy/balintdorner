83c6630385a342df039d1f9928e82877feaf2e82
Balint Dorner <dorner.balint@gmail.com>
2017-07-03 20:37:09 +0200

    feat(practice):hello world

---
+++ week-07/practice.js
@@ -0,0 +1,11 @@
+'use strict';
+
+console.log('Hello World!');
+console.log('Hello Petra!');
+console.log('Hello Bajusz!');
+console.log('Hello Fanni!');
+
+console.log('Humpty Dumpty had a great fall.');
+console.log('Humpty Dumpty sat on a wall,');
+console.log('All the king\'s horses and all the king\'s men');
+console.log('Couldn\'t put Humpty together again.');
