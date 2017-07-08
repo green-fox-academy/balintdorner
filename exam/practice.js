8d3fc401d603c8abf9256840f7ef49ebfff71e1c
Balint Dorner <dorner.balint@gmail.com>
2017-07-08 18:52:47 +0200

    feat(splice, foreach): added examples

--- exam/practice.js
+++ exam/practice.js
@@ -24,4 +24,17 @@ let example = function(x) {
 };
 
 // Higher order functions -
-//Functions that operate on other functions, either by taking them as arguments or by returning them, are called higher-order functions. 
+//Functions that operate on other functions, either by taking them as arguments or by returning them, are called higher-order functions.
+// i.e.: filter, map
+
+//built in methods splice, forEach, map, filter
+var names = ['Bela', 'Atesz', 'Gabi', 'Jani', 'Peti', 'Gyuri']
+
+//array.splice(honnan, mennyit, insertItem1, insertItem2, ...)
+names.splice(2,4, 'Tibi')
+//names = ['Bela', 'Atesz', 'Tibi']
+
+//arr.forEach(function(elment, index, teljes lista)
+names.forEach(function(element) {
+    console.log(element);
+});
