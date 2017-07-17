da316b971fb3167ce112783502b6cb1edd1e181a
Balint Dorner <dorner.balint@gmail.com>
2017-07-17 14:34:45 +0200

    feat(algorithm): exercise one, done

---
+++ week-17/day-01/exercises.js
@@ -0,0 +1,16 @@
+// If we list all the natural numbers below 10 that are multiples of 3 or 5, we get 3, 5, 6 and 9. The sum of these multiples is 23.
+
+// Find the sum of all the multiples of 3 or 5 below 1000.
+
+function summer() {
+    let total = 0;
+    for(let i = 1; i < 1000; i++) {
+        if( i % 3 === 0 || i % 5 === 0 ) {
+            total += i;
+        }
+    }
+    console.log(total)
+}
+
+summer()
+console.log('alma')
---
+++ week-17/day-01/notes.js
@@ -0,0 +1,32 @@
+/*
+Big o notation: komplexitasi szint, adott szamu muvelet elvegzese mennyi ideig tart - futasi ido -
+
+Hash table: 
+key value pairs
+
+Linked list: 
+data, next lanculat
+
+Double linked list: 
+hasonlo mint az elozo, csak ez mindket iranyba bejarhato
+
+Tree:
+Grafszeru, de az halozati,
+mig a fa hierarchikus szerkezetu
+Csak szulo gyerek kapcsolat van
+A DOM pl egy fa
+
+Binary search:
+Binaris fan keresunk. 
+
+DFS BFS:
+grafban lehet veluk keresni
+BFS: a leghosszabb nodeot jarja be mindig, egy agon megy vegig eloszor
+BFS: hozzank legkozelebbi nodeokat jarja be
+
+Two's complement element
+Binarisan, hogyan abrazoljuk az elemeket
+0 = + | 1 = -
+
+Logical shif
+
